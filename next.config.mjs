47ce11ed6604d58a5515709c433d3bb3528c9a58
Williston Nunes <willistonsousanunes@hotmail.com>
2026-07-05 23:39:22 -0300

    init

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,4 @@
-/** @type {import('next').NextConfig} */
+/** @type {import('next').NextConfig} 
 const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
@@ -9,3 +9,16 @@ const nextConfig = {
 }
 
 export default nextConfig
+**/
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export', // Ativa a exportação estática
+  basePath: '/nome-do-seu-repositorio', // Substitua pelo nome exato do seu repo no GitHub
+  assetPrefix: '/nome-do-seu-repositorio/', // Substitua pelo nome exato do seu repo no GitHub
+  images: {
+    unoptimized: true, // Necessário pois o GitHub Pages não suporta otimização de imagens do Next.js
+  },
+};
+
+module.exports = nextConfig;
