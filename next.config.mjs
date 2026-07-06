a9e45c1096b94ee965611f2ebf944fb56e697f0d
Williston Nunes <willistonsousanunes@hotmail.com>
2026-07-05 23:44:36 -0300

    init

--- next.config.mjs
+++ next.config.mjs
@@ -14,8 +14,8 @@ export default nextConfig
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export', // Ativa a exportação estática
-  basePath: '/nome-do-seu-repositorio', // Substitua pelo nome exato do seu repo no GitHub
-  assetPrefix: '/nome-do-seu-repositorio/', // Substitua pelo nome exato do seu repo no GitHub
+  basePath: '/portifolio', // Substitua pelo nome exato do seu repo no GitHub
+  assetPrefix: '/portifolio/', // Substitua pelo nome exato do seu repo no GitHub
   images: {
     unoptimized: true, // Necessário pois o GitHub Pages não suporta otimização de imagens do Next.js
   },
