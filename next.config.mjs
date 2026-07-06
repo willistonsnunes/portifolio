230ce6b6f34c4a5090f8c5b52dafa8a5969718f3
Williston Nunes <willistonsousanunes@hotmail.com>
2026-07-05 23:53:59 -0300

    init

--- next.config.mjs
+++ next.config.mjs
@@ -11,7 +11,7 @@ const nextConfig = {
 export default nextConfig
 **/
 
-/** @type {import('next').NextConfig} */
+/** @type {import('next').NextConfig} 
 const nextConfig = {
   output: 'export', // Ativa a exportação estática
   basePath: '/portifolio', // Substitua pelo nome exato do seu repo no GitHub
@@ -20,5 +20,17 @@ const nextConfig = {
     unoptimized: true, // Necessário pois o GitHub Pages não suporta otimização de imagens do Next.js
   },
 };
+**/
 
 module.exports = nextConfig;
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  // Adicione basePath e assetPrefix apenas se o nome do seu repo NÃO for seu-usuario.github.io
+   basePath: '/portifolio',
+};
+
+export default nextConfig; // ✅ Forma correta para .mjs
